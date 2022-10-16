139d73eb61fcf5ebbfc9809dc77e24f283815b85
Danila Rogozhnikov <94745416+m0libdEn@users.noreply.github.com>
2022-10-16 19:39:49 -0400

    Version 1.1 (fixed bugs)
    1. Fixed a bug when clicking on the field popped up a victory notification
    2. The winning side is now visible in the victory notification
    3. A notification about the night appears if it happened
    4. After the end of the game, a button pops up to start a new game, after clicking on it, the page reloads
    5. Adaptive has been fixed, now the game has become more convenient on all devices

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Get all squares of field
 const square1 = document.getElementById('square-1__info')
 const square2 = document.getElementById('square-2__info')
 const square3 = document.getElementById('square-3_info')
@@ -10,88 +11,134 @@ const square7 = document.getElementById('square-7__info')
 const square8 = document.getElementById('square-8__info')
 const square9 = document.getElementById('square-9__info')
 
-const header = document.getElementById('header')
-let PLAYERS = true
+const header = document.getElementById('header') //Get element for change text for winner
 
-let arrOfMoves = new Array(3)
+const playagain = document.getElementById('playagain')//Get element for ask to play again after game
+
+let PLAYERS = true //First player - x - true, Second Player - o - false
+let arrOfMoves = new Array(3) 
 for (let i = 0; i < arrOfMoves.length; i++) {
     arrOfMoves[i] =  new Array(3)
-}
+} //Create array of moves
 
-function checkWin(){
-    for (let i = 0; i < arrOfMoves.length; i++) {
-        if (arrOfMoves[i][0] === arrOfMoves[i][1] && arrOfMoves[i][1] === arrOfMoves[i][2] && arrOfMoves[i][0]!==undefined) {
-            header.innerHTML = 'Win'
-        }
-    }
-
-    for (let i = 0; i < arrOfMoves.length; i++) {
-        if (arrOfMoves[0][i] === arrOfMoves[1][i] && arrOfMoves[1][i] === arrOfMoves[2][i] && arrOfMoves[0][i]!==undefined) {
-            header.innerHTML = 'Win'
-        }
-    }
-
-    if((arrOfMoves[0][0] === arrOfMoves[1][1] && arrOfMoves[1][1] === arrOfMoves[2][2] ) || (arrOfMoves[0][2] === arrOfMoves[1][1] && arrOfMoves[1][1] === arrOfMoves[2][0]) && arrOfMoves[1][1] !==undefined){
-        header.innerHTML = 'Win'
-    }
-}
-
-function checkMove(square){return square.textContent?true:false}
 
 function makeMove(square) {
   if (!checkMove(square)) {
-    if (PLAYERS) {
-        square.innerHTML = 'X'
-        PLAYERS = false
+    if (PLAYERS) { //if PLAYER == TRUE  -> that's x turn
+        square.innerHTML = 'X' //put 'X' on field
+        PLAYERS = false //change turn
     }
     else{
         square.innerHTML = 'O'
         PLAYERS = true
   }
-  return square.textContent
+  return square.textContent //return our move to put it in our array
 }
 }
 
+function checkMove(square){return square.textContent?true:false} //checking if field have have space for move
+
+
+function checkGame(){ //this function check if game finshed  - it always any variable  - true - that's mean that game finiched, false - continue
+    //check horizontal 
+    for (let i = 0; i < arrOfMoves.length; i++) {
+           if (arrOfMoves[i][0] === arrOfMoves[i][1] && arrOfMoves[i][1] === arrOfMoves[i][2] && arrOfMoves[i][0]!==undefined && arrOfMoves[i][1]!==undefined && arrOfMoves[i][2]!==undefined) {
+               header.innerHTML = `Winner is ${arrOfMoves[i][0]}` //change our header 
+               return true
+           }
+       }
+    //check vertical
+       for (let i = 0; i < arrOfMoves.length; i++) {
+           if (arrOfMoves[0][i] === arrOfMoves[1][i] && arrOfMoves[1][i] === arrOfMoves[2][i] && arrOfMoves[0][i]!==undefined && arrOfMoves[1][i]!==undefined && arrOfMoves[2][i]!==undefined) {
+               header.innerHTML = `Winner is ${arrOfMoves[0][i]}`
+               return true
+           }
+       }
+    //check diagonal
+       if((arrOfMoves[0][0] === arrOfMoves[1][1] && arrOfMoves[1][1] === arrOfMoves[2][2] && arrOfMoves[1][1] !==undefined ) || (arrOfMoves[0][2] === arrOfMoves[1][1] && arrOfMoves[1][1] === arrOfMoves[2][0] && arrOfMoves[1][1] !==undefined)){
+           header.innerHTML = `Winner is ${arrOfMoves[1][1]}`
+           return true 
+       }
+   
+       //check tie
+       let tie = true //make variable do we have a tie (default true)
+       for (let i = 0; i < arrOfMoves.length; i++) {
+          for (let k = 0; k < arrOfMoves.length; k++) {
+               if (arrOfMoves[i][k] == null && arrOfMoves[i][k]==undefined) { //if ANY OF FIELDS EMPLTY and NO ANY neither side won - chage to false
+                   tie = false    
+               }
+          } 
+       }
+       if (tie) { //if all squares are not empty, but game not finched  - change header
+           header.innerHTML='Tie, try more one time!'
+           return true
+       }
+   
+       return false
+   }
+   
+   function checkEndOfGame(){
+       if (checkGame()) {
+           playagain.style.display='block' //if game ends,then the continue button pops up
+       }
+   }
+
 square1.addEventListener('click',function () {
-    arrOfMoves[0][0] = makeMove(square1)
-    checkWin()
+    if (!(checkEndOfGame())) {
+        arrOfMoves[0][0] = makeMove(square1) //add our move of player (x or o) to our array
+        checkEndOfGame() 
+    }
 })
 square2.addEventListener('click',function () {
-    arrOfMoves[0][1] = makeMove(square2)
-    checkWin()
+    if (!(checkEndOfGame())) {
+        arrOfMoves[0][1] = makeMove(square2)
+        checkEndOfGame() 
+    }
 })
 square3.addEventListener('click',function () {
-    arrOfMoves[0][2] = makeMove(square3)
-    console.log(arrOfMoves);
-    checkWin()
+    if (!(checkEndOfGame())) {
+        arrOfMoves[0][2] = makeMove(square3)
+        checkEndOfGame() 
+    }
 })
 square4.addEventListener('click',function () {
-    arrOfMoves[1][0] = makeMove(square4)
-    console.log(arrOfMoves);
-    checkWin()
+    if (!(checkEndOfGame())) {
+        arrOfMoves[1][0] = makeMove(square4)
+        checkEndOfGame() 
+    }
 })
 square5.addEventListener('click',function () {
-    arrOfMoves[1][1] = makeMove(square5)
-    console.log(arrOfMoves);
-    checkWin()
+    if (!(checkEndOfGame())) {
+        arrOfMoves[1][1] = makeMove(square5)
+        checkEndOfGame() 
+    }
 })
 square6.addEventListener('click',function () {
-    arrOfMoves[1][2] = makeMove(square6)
-    console.log(arrOfMoves);
-    checkWin()
+    if (!(checkEndOfGame())) {
+        arrOfMoves[1][2] = makeMove(square6)
+        checkEndOfGame() 
+    }
 })
 square7.addEventListener('click',function () {
-    arrOfMoves[2][0] = makeMove(square7)
-    console.log(arrOfMoves);
-    checkWin()
+    if (!(checkEndOfGame())) {
+        arrOfMoves[2][0] = makeMove(square7)
+        checkEndOfGame() 
+    }
 })
 square8.addEventListener('click',function () {
-    arrOfMoves[2][1] = makeMove(square8)
-    console.log(arrOfMoves);
-    checkWin()
+    if (!(checkEndOfGame())) {
+        arrOfMoves[2][1] = makeMove(square8)
+        checkEndOfGame() 
+    }
 })
 square9.addEventListener('click',function () {
-    arrOfMoves[2][2] = makeMove(square9)
-    console.log(arrOfMoves);
-    checkWin()
+    if (!(checkEndOfGame())) {
+        arrOfMoves[2][2] = makeMove(square9)
+        checkEndOfGame() 
+    }
+})
+
+
+playagain.addEventListener('click', function(){
+    window.location.reload() //if user put the button - game is restarted
 })
